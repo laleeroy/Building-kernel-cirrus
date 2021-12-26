27d11443b67a8617a78ffad7614236e0e2df748f
NFS86 <jarbull86@gmail.com>
2021-12-26 20:15:35 +0700

    😎
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- eleven/rm.sh
+++ eleven/rm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd $PWD/${KERNEL_ROOTDIR}
-make O=out clean && make O=out mrproper && rm -rf AnyKernel
+make O=out clean && make O=out mrproper && rm -rf $PWD/AnyKernel
 cd $PWD
