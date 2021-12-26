af1f33714aa98e4dac6142ddda41724e9a96591a
NFS86 <jarbull86@gmail.com>
2021-12-26 19:26:34 +0700

    fix directory hcc toolchain
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- eleven/download.sh
+++ eleven/download.sh
@@ -5,5 +5,5 @@ echo "Downloading few Dependecies . . ."
 git clone --depth=1 https://github.com/NFS-projects/kernel_xiaomi_rosy -b eleven $PWD/$DEVICE_CODENAME
 # Toolchain
 git clone --depth=1 https://github.com/NFS-projects/gcc-arm -b 11.x $PWD/GCC32
-git clone --depth=1 https://github.com/NFS-projects/gcc-arm64 -b 11.x tmp/GCC64
+git clone --depth=1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $PWD/GCC64
 git clone --depth=1 https://github.com/AnGgIt88/Finix-Clang -b 14.x $PWD/CLANG
