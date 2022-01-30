e192659944e2b5ddd0b43b4a66a8d405bf2ca1bb
NFS86 <jarbull86@gmail.com>
2022-01-30 20:50:25 +0700

    Test witch GCC 12
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- eleven/download.sh
+++ eleven/download.sh
@@ -4,6 +4,6 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/NFS86/kernel_xiaomi_rosy -b twelve $pwd/$DEVICE_CODENAME
 # Toolchain
-git clone --depth=1 https://github.com/NFS-projects/gcc-arm -b 11.x $pwd/GCC32
-git clone --depth=1 https://github.com/NFS-projects/gcc-arm64 -b 11.x $pwd/GCC64
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $pwd/GCC32
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master $pwd/GCC64
 git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master $pwd/CLANG
