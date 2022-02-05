f394460e2530ad60e087e05aa199450fd2b90117
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-05 18:00:17 +0600

    fix error 2

--- download.sh
+++ download.sh
@@ -6,4 +6,3 @@ git clone --depth 1 https://github.com/nhAsif/kernel_xiaomi_rosy -b twelve $CIRR
 # Toolchain
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
-git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
