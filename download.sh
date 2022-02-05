340b448e468e57a3afdd6f2b48b329d430cbfb13
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-05 17:21:18 +0600

    use shallow clone

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth 1 https://github.com/nhAsif/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-git clone https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
-git clone https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
+git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
+git clone --depth 1 https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
 git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
