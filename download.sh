42b22f5dff5257d92df124ddfdc04881f8580971
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-05 17:09:00 +0600

    test build 1

--- download.sh
+++ download.sh
@@ -2,8 +2,8 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth 1 https://github.com/NFS86/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/nhAsif/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm -b 12.x $CIRRUS_WORKING_DIR/GCC32
-git clone --depth 1 https://github.com/NFS-projects/gcc-arm64 -b 12.x $CIRRUS_WORKING_DIR/GCC64
+git clone https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
+git clone https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
 git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
