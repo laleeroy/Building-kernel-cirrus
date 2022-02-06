0e264e0b9cc6b3c45d32e8c107e6ad3b46210b2c
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-06 10:00:21 +0600

    fixed all errors

--- download.sh
+++ download.sh
@@ -4,5 +4,6 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth 1 https://github.com/nhAsif/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
+
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
