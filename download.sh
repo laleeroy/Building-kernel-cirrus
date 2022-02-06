2a018a172152581ea5f218f5d642ca7145fd53f2
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-06 17:41:21 +0600

    update kernel

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth 1 https://github.com/nhAsif/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/54b1d/kernel_xiaomi_rosy -b tortoise297 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
 
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
