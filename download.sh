fe54e47e41557d6aa4231ee4927ed22a9438a5d0
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-08 23:01:43 +0600

    update [skip ci]

--- download.sh
+++ download.sh
@@ -2,8 +2,6 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth 1 https://github.com/NFS86/kernel_xiaomi_rosy -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/baunilla/android_kernel_xiaomi_rosy -b lineage-19.0 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
-
-git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
-git clone --depth 1 https://github.com/Pulkit077/gcc-arm64 -b gcc-master $CIRRUS_WORKING_DIR/GCC64
+git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
