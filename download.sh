615637a728069016d2313d3f87a9369c10370e8d
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-21 10:42:34 +0600

    test baunilla [skip ci]

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading few Dependecies . . ."
 # Kernel Sources
-git clone --depth 1 https://github.com/NFS-projects/kernel_xiaomi_rosy.git -b twelve $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
+git clone --depth 1 https://github.com/baunilla/android_kernel_xiaomi_rosy.git -b lineage-19.0 $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
 # Toolchain
 git clone --depth 1 https://gitlab.com/AnGgIt86/nfs-clang.git -b main $CIRRUS_WORKING_DIR/CLANG
 git clone --depth 1 https://github.com/Pulkit077/gcc-arm -b gcc-master $CIRRUS_WORKING_DIR/GCC32
